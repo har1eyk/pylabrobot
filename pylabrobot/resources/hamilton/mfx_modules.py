d070df7d5dbd040769ba86010db30a7729c73495
Harley King <harleyk@gmail.com>
2025-09-18 15:12:59 -0400

    188042 with 10mm 3D printed adapters added

--- pylabrobot/resources/hamilton/mfx_modules.py
+++ pylabrobot/resources/hamilton/mfx_modules.py
@@ -58,6 +58,23 @@ def Hamilton_MFX_plateholder_DWP_metal_tapped(name: str) -> PlateHolder:
     model=Hamilton_MFX_plateholder_DWP_metal_tapped.__name__,
   )
 
+def Hamilton_MFX_plateholder_DWP_metal_tapped_10mm_3dprint(name: str) -> PlateHolder:
+  """Hamilton MFX DWP Module (cat.-no. 188042 / 188042-00).
+  It also contains metal clamps at the corners.
+  https://www.hamiltoncompany.com/other-robotics/188042
+  """
+
+  return PlateHolder(
+    name=name,
+    size_x=135.0,  # measured
+    size_y=94.0,  # measured
+    size_z=29.8,  # measured
+    # probe height - carrier_height - deck_height
+    child_location=Coordinate(4.0, 4.0, 183.95 - 63.95 - 100),  # 20.0 measured
+    pedestal_size_z=-4.74,
+    model=Hamilton_MFX_plateholder_DWP_metal_tapped_10mm_3dprint.__name__,
+  )
+
 
 def MFX_DWP_module_flat(name: str) -> PlateHolder:
   """Hamilton cat. no.: 6601988-01
